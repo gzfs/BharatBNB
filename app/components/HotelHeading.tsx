745ad0bfe7a0a28df790d42d7561e6d678cd6dc9
gzfs <vismbs@pm.me>
2023-09-15 21:52:34 +0530

    Added HotelHeading.tsx

---
+++ app/components/HotelHeading.tsx
@@ -0,0 +1,30 @@
+import { capitalizeString } from "../utils/Helper.funcs";
+
+export default function HotelHeading({
+  districtName,
+}: {
+  districtName: string;
+}) {
+  return (
+    <div className="relative">
+      <img src="/assets/imgs/dummy/Beach.jpg" className="" />
+      <div className="bg-black w-full absolute top-0 left-0 h-full opacity-60" />
+      <div className="w-full absolute top-0 left-0 h-full vignette" />
+      <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center font-Coolvetica text-white">
+        <div className="leading-[100px]">
+          <p className="text-xl">Welcome to</p>
+          <p className="text-[100px]">
+            {capitalizeString(districtName)
+              .split("-")
+              .map((seperateString) => {
+                return capitalizeString(seperateString);
+              })
+              .join(" ")}
+            .
+          </p>
+          <p className="text-xl mt-2">Vaa Mame Fun Panlam 🔥</p>
+        </div>
+      </div>
+    </div>
+  );
+}
