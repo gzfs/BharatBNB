23f66494d6a192796abfebeba9aaf0f8bd2e7232
gzfs <vismbs@pm.me>
2023-09-16 05:59:02 +0530

    Routing to the Single Hotel Page when Clicked

--- app/components/HotelCard.tsx
+++ app/components/HotelCard.tsx
@@ -1,9 +1,13 @@
-export default function HotelCard() {
+import Link from "next/link";
+
+export default function HotelCard({ hotelID }: { hotelID: string }) {
   return (
     <div className="font-Coolvetica flex flex-col items-center">
       <div className="relative">
         <div className="absolute"></div>
-        <img src="/assets/imgs/dummy/Bruh.png" width="250px" />
+        <Link href={`/hotel/${hotelID}`}>
+          <img src="/assets/imgs/dummy/Bruh.png" width="250px" />
+        </Link>
         <div className="mt-4">
           <p className="text-lg">Rajbhavan, Chennai</p>
           <p className="text-lg text-[#AC8080]">
