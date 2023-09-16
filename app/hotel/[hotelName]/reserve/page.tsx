764daf4027452d84c783593a685d616f7a6a7e12
gzfs <vismbs@pm.me>
2023-09-16 09:52:26 +0530

    Misc

--- app/hotel/[hotelName]/reserve/page.tsx
+++ app/hotel/[hotelName]/reserve/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import Auth from "@/app/components/Auth";
+import Divider from "@/app/components/Divider";
 import ReserveDetails from "@/app/components/ReserveDetails";
+import ReserveHotelCard from "@/app/components/ReserveHotelCard";
 import Link from "next/link";
 import { useState } from "react";
 
@@ -19,47 +22,24 @@ export default function ReservePage() {
           Request to Book
         </p>
       </div>
-      <ReserveDetails
-        checkInDate={checkInDate}
-        setCheckInDate={setCheckInDate}
-        checkOutDate={checkOutDate}
-        setCheckOutDate={setCheckOutDate}
-        adultCount={adultCount}
-        setAdultCount={setAdultCount}
-        infantCount={infantCount}
-        setInfantCount={setInfantCount}
-        petCount={petCount}
-        setPetCount={setPetCount}
-      />
-      <div className="w-[400px] flex justify-center py-8">
-        <div className="bg-[#F6762D] h-[3px] rounded-xl w-3/12"></div>
-      </div>
-      <p className="text-4xl font-Coolvetica text-[#4BAF32] pb-6">
-        Sign in with
-      </p>
-      <div className="flex w-[400px] justify-start">
-        <Link
-          href="#"
-          className="font-Coolvetica text-[#F6762D] text-xl border-2 border-[#F6762D] px-2 py-2 w-[160px] rounded-full flex items-center justify-start"
-        >
-          <img
-            src="/assets/imgs/icons/Biometric.svg"
-            alt="Google"
-            width="30px"
+      <div className="flex w-full">
+        <div className="w-6/12">
+          <ReserveDetails
+            checkInDate={checkInDate}
+            setCheckInDate={setCheckInDate}
+            checkOutDate={checkOutDate}
+            setCheckOutDate={setCheckOutDate}
+            adultCount={adultCount}
+            setAdultCount={setAdultCount}
+            infantCount={infantCount}
+            setInfantCount={setInfantCount}
+            petCount={petCount}
+            setPetCount={setPetCount}
           />
-          <p className="ml-4">Digilocker</p>
-        </Link>
-        <Link
-          href="#"
-          className="font-Coolvetica ml-4 text-[#F6762D] text-xl border-2 border-[#F6762D] px-2 py-2 w-[160px] rounded-full flex items-center justify-start"
-        >
-          <img
-            src="/assets/imgs/icons/Google.svg"
-            alt="Google"
-            width="30px"
-          />
-          <p className="ml-4">Google</p>
-        </Link>
+          <Divider />
+          <Auth />
+        </div>
+        <ReserveHotelCard />
       </div>
     </main>
   );
