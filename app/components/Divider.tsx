9a24f52c4858e13072b5979c5998d102db2fae3a
gzfs <vismbs@pm.me>
2023-09-16 09:50:25 +0530

    Created Divider.tsx

---
+++ app/components/Divider.tsx
@@ -0,0 +1,7 @@
+export default function Divider() {
+  return (
+    <div className="w-[400px] flex justify-center py-8">
+      <div className="bg-[#F6762D] h-[3px] rounded-xl w-3/12"></div>
+    </div>
+  );
+}
