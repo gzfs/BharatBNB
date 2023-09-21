5b4c8d9d7fe52591ea707126b784d70d06a9304e
Adhithya Srivatsan <addytrunks@gmail.com>
2023-09-21 19:21:51 +0530

    state management + local storage

--- hooks/useListingStore.ts
+++ hooks/useListingStore.ts
@@ -1,33 +1,36 @@
-import { Address, Amenities, Coordinates, Images } from "@prisma/client"
-import { create } from "zustand"
+import { Address, Amenities, Coordinates, Images } from "@prisma/client";
+import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface ListingStore {
-    coordinatesLattitude: number,
-    coordinatesLongitude: number,
-    Description: string,
-    Name: string,
-    pricePerNight: number,
-    Address: Address,
-    Images: Images[],
-    Amenities: Amenities[],
-    StructureType: string,
-    PrivacyType: string,
-    setNameAndDesc: ({ name, desc }: { name: string, desc: string }) => void,
-    setAddress: (addressObj: Address) => void,
-    setCoords: (coordsObj: Coordinates) => void,
-    setPrice: (price: number) => void,
-    setImages: (Images: Images[]) => void,
-    setAmenities: (amenityList: Amenities[]) => void
-    setPrivacyType: (privacyType: string) => void
-    setStructureType: (structureType: string) => void
+  coordinatesLattitude: number;
+  coordinatesLongitude: number;
+  Description: string;
+  Name: string;
+  pricePerNight: number;
+  Address: Address;
+  Images: Images[];
+  Amenities: Amenities[];
+  StructureType: string;
+  PrivacyType: string;
+  setNameAndDesc: ({ name, desc }: { name: string; desc: string }) => void;
+  setAddress: (addressObj: Address) => void;
+  setCoords: (coordsObj: Coordinates) => void;
+  setPrice: (price: number) => void;
+  setImages: (Images: Images[]) => void;
+  setAmenities: (amenityList: Amenities[]) => void;
+  setPrivacyType: (privacyType: string) => void;
+  setStructureType: (structureType: string) => void;
 }
 
-export const useListingStore = create<ListingStore>((set) => ({
-    coordinatesLattitude: 0.0,
-    coordinatesLongitude: 0.0,
-    Description: "",
-    Name: "",
-    Address: {
+export const useListingStore = create(
+  persist<ListingStore>(
+    (set) => ({
+      coordinatesLattitude: 0.0,
+      coordinatesLongitude: 0.0,
+      Description: "",
+      Name: "",
+      Address: {
         City: "",
         County: "",
         HouseName: "",
@@ -35,38 +38,48 @@ export const useListingStore = create<ListingStore>((set) => ({
         ID: "",
         Pincode: "",
         StreetAddress: "",
-        Landmark: ""
-    },
-    Amenities: [],
-    Images: [],
-    pricePerNight: 0,
-    StructureType: "",
-    PrivacyType: "",
-    setNameAndDesc: ({ name, desc }: { name: string, desc: string }) => set({
-        Name: name,
-        Description: desc
+        Landmark: "",
+      },
+      Amenities: [],
+      Images: [],
+      pricePerNight: 0,
+      StructureType: "",
+      PrivacyType: "",
+      setNameAndDesc: ({ name, desc }: { name: string; desc: string }) =>
+        set({
+          Name: name,
+          Description: desc,
+        }),
+      setAddress: (addressObj: Address) =>
+        set({
+          Address: addressObj,
+        }),
+      setCoords: (coordsObj: Coordinates) =>
+        set({
+          coordinatesLattitude: coordsObj.Lattitude,
+          coordinatesLongitude: coordsObj.Longitude,
+        }),
+      setPrice: (price: number) =>
+        set({
+          pricePerNight: price,
+        }),
+      setImages: (Images: Images[]) =>
+        set({
+          Images: Images,
+        }),
+      setAmenities: (amenityList: Amenities[]) =>
+        set({
+          Amenities: amenityList,
+        }),
+      setPrivacyType: (privacyType: string) =>
+        set({
+          PrivacyType: privacyType,
+        }),
+      setStructureType: (structureType: string) =>
+        set({
+          StructureType: structureType,
+        }),
     }),
-    setAddress: (addressObj: Address) => set({
-        Address: addressObj
-    }),
-    setCoords: (coordsObj: Coordinates) => set({
-        coordinatesLattitude: coordsObj.Lattitude,
-        coordinatesLongitude: coordsObj.Longitude
-    }),
-    setPrice: (price: number) => set({
-        pricePerNight: price
-    }),
-    setImages: (Images: Images[]) => set({
-        Images: Images
-    }),
-    setAmenities: (amenityList: Amenities[]) => set({
-        Amenities: amenityList
-    }),
-    setPrivacyType: (privacyType: string) => set({
-        PrivacyType: privacyType
-    }),
-    setStructureType: (structureType: string) => set({
-        StructureType: structureType
-    }),
-
-}))
+    { name: "listingStore", storage: createJSONStorage(() => localStorage) }
+  )
+);
