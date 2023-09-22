2569e2eb60a80c36c028036770aab97a20d648e1
Vishal <vismbs@pm.me>
2023-09-22 14:19:50 +0530

    Chatbot completed

--- components/modals/chatbotModal.tsx
+++ components/modals/chatbotModal.tsx
@@ -21,7 +21,7 @@ type Message = {
 const ChatBotModal = () => {
   const { isOpen, onClose, type } = useModalStore();
 
-  const [isLoading,setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
   const [messages, setMessages] = useState<Message[]>([]);
   const [userMessage, setUserMessage] = useState("");
 
@@ -37,29 +37,33 @@ const ChatBotModal = () => {
       body: JSON.stringify({ userPrompt: userMessage }),
     });
     const data = await res.json()
-    setMessages((prev) => [...prev,{role:'bot',message:data?.chatCompletion[0]?.message?.content!}])
+    setMessages((prev) => [...prev, { role: 'bot', message: data?.chatCompletion[0]?.message?.content! }])
     setIsLoading(false)
   };
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
-      <DialogContent className="bg-white text-black overflow-hidden p-0">
+      <DialogContent className="bg-white text-black overflow-hidden p-0 max-w-5xl">
         <DialogHeader className="pt-8">
           <DialogTitle className="text-3xl text-center font-normal font-Coolvetica text-[#4BAF32]">
-            ChatBot
+            bharatbnb chatbot
           </DialogTitle>
           <DialogDescription className="text-center text-zinc-500 font-Coolvetica">
             Get your personalized itineary.
           </DialogDescription>
         </DialogHeader>
 
-        <div className="flex flex-col px-2">
+        <div className="flex flex-col px-2 h-[70vh]">
           <div className="flex-1 overflow-y-scroll">
             {messages?.map((message) => (
-                <p key={message.role}>{message.message}</p>
+              <p key={message.role} style={{
+                justifyContent: (message.role === "bot") ? "start" : "end"
+              }} className="flex"><p className="px-6 py-3 w-fit border-2 rounded-full my-2" style={{
+                borderColor: (message.role === "bot") ? "#4BAF32" : "#F6762D"
+              }}>{message.message}</p></p>
             ))}
           </div>
-          <div className="flex">
+          <div className="flex p-3">
             <input
               className="flex-1 border-0 outline-none p-2"
               value={userMessage}
